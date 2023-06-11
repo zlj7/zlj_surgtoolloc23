2d12a1f8b5be930ce10c7330f34e0eb8308b0afd
zlj7 <1274227493@qq.com>
2023-06-11 20:58:59 +0800

    add surtooldataset and gen_txt

---
+++ tools/gen_txt.py
@@ -0,0 +1,27 @@
+import os
+import random
+
+# 定义图片目录和输出目录
+image_dir = '/data2/zhenglujie/data/SurtoolDataset/images/'
+output_dir = '/data2/zhenglujie/data/SurtoolDataset/'
+
+# 获取所有图片的路径
+images = os.listdir(image_dir)
+image_paths = [os.path.join(image_dir, img) for img in images]
+
+# 随机打乱图片路径列表
+random.shuffle(image_paths)
+
+# 计算训练集和测试集的分割点
+split = int(0.8 * len(image_paths)) # 80%用作训练集，20%用作测试集
+
+# 将图片路径列表分割成训练集和测试集
+train_paths = image_paths[:split]
+test_paths = image_paths[split:]
+
+# 将训练集和测试集的图片路径写入train.txt和test.txt文件中
+with open(os.path.join(output_dir, 'train.txt'), 'w') as f:
+    f.write('\n'.join(train_paths))
+
+with open(os.path.join(output_dir, 'test.txt'), 'w') as f:
+    f.write('\n'.join(test_paths))
---
+++ tools/surtooldataset.py
@@ -0,0 +1,84 @@
+import os
+import pandas as pd
+import numpy as np
+import cv2
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class SurtoolDataset(Dataset):
+    def __init__(self, txt_file, csv_file, transform=None):
+        self.image_paths = []
+        self.labels = []
+        self.transform = transform
+
+        # 读取train.txt文件中的图片路径
+        with open(txt_file, 'r') as f:
+            for line in f.readlines():
+                self.image_paths.append(line.strip())
+
+        # 读取labels.csv文件中的标签
+        df = pd.read_csv(csv_file)
+        for path in self.image_paths:
+            filename = os.path.basename(path)
+            clip_name = filename.split("-")[0]
+            label_str = df.loc[df['clip_name'] == clip_name, 'tools_present'].values[0]
+            label_list = label_str[1 : -1].split(', ')
+            label = [self.map_label(l) for l in label_list if l != "nan"]
+            print(label)
+            self.labels.append(label)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        # 读取图像
+        img = cv2.imread(self.image_paths[idx])
+        # print(img)
+        # img = cv2.resize(img, (self.img_width, self.img_height))
+        img = img / 255.0
+        img = img.transpose(2, 0, 1)
+
+        # 数据增强
+        if self.transform:
+            img = self.transform(img)
+
+        return{
+            'imgs': img.astype(np.float64),
+            'label': self.labels[idx]
+        }
+
+    def map_label(self, label_name):
+        # 将类别名称映射为数字标签
+        label_map = {
+            'needle driver': 0,
+            'cadiere forceps': 1,
+            'prograsp forceps': 2,
+            'grasping retractor': 3,
+            'clip applier ': 4,
+            'bipolar dissector': 5,
+            'bipolar forceps': 6,
+            'stapler': 7,
+            'monopolar curved scissors': 8,
+            'force bipolar': 9,
+            'permanent cautery hook/spatula': 10,
+            'suction irrigator': 11,
+            'vessel sealer': 12,
+            'tip-up fenestrated grasper': 13
+        }
+        return label_map[label_name]
+
+
+
+# 创建数据集
+train_data = SurtoolDataset('../../data/SurtoolDataset/train.txt', '../../data/SurtoolDataset/labels.csv', transform=None)
+test_data = SurtoolDataset('../../data/SurtoolDataset/test.txt', '../../data/SurtoolDataset/labels.csv', transform=None)
+
+# 创建数据加载器
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+
+for step, batch in enumerate(train_loader):
+    imgs = batch['imgs']
+    label = batch['label']
+    # print(imgs)
+    print(label)
